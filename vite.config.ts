a4d8e2c4cbd69a6910f46a37bcd4081ef6a122d2
rizkydwiyuliyanto <dwiyuliyantorizky86@gmail.com>
2025-01-24 16:32:02 +0900

    test-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,42 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'AppName',
+        description: 'Your app description',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
     tsconfigPaths(),
     checker({
       typescript: true,
